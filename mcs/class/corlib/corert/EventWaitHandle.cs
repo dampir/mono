48881a31f24d09cb591bd97f240deb14e3c475ca
Alexander Efremov <aedampir@gmail.com>
2017-06-22 23:39:13 +0700

    [corert] Integrated eventwaithandle, manualresetevent, autoresetevent, mutex and semaphore from corert.

---
+++ mcs/class/corlib/corert/EventWaitHandle.cs
@@ -0,0 +1,65 @@
+﻿namespace System.Threading
+{
+	public partial class EventWaitHandle
+	{
+		private static void VerifyNameForCreate (string name)
+		{
+			if (Environment.IsRunningOnWindows)
+			{
+				Windows_VerifyNameForCreate (name);
+			}
+			else
+			{
+				Unix_VerifyNameForCreate (name);
+			}
+		}
+
+		private void CreateEventCore (bool initialState, EventResetMode mode, string name, out bool createdNew)
+		{
+			if (Environment.IsRunningOnWindows)
+			{
+				Windows_CreateEventCore (initialState, mode, name, out createdNew);
+			}
+			else
+			{
+				Unix_CreateEventCore (initialState, mode, name, out createdNew);
+			}
+		}
+
+		private static OpenExistingResult OpenExistingWorker (string name, out EventWaitHandle result)
+		{
+			if (Environment.IsRunningOnWindows)
+			{
+				return Windows_OpenExistingWorker (name, out result);
+			}
+			else
+			{
+				return Unix_OpenExistingWorker (name, out result);
+			}
+		}
+
+		private static bool ResetCore (IntPtr handle)
+		{
+			if (Environment.IsRunningOnWindows)
+			{
+				return Windows_ResetCore (handle);
+			}
+			else
+			{
+				return Unix_ResetCore (handle);
+			}
+		}
+
+		private static bool SetCore (IntPtr handle)
+		{
+			if (Environment.IsRunningOnWindows)
+			{
+				return Windows_SetCore (handle);
+			}
+			else
+			{
+				return Unix_SetCore (handle);
+			}
+		}
+	}
+}
